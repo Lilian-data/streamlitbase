0893fe30d7a292da3890cbae17144c33e2656796
Lilian-data <136447537+Lilian-data@users.noreply.github.com>
2023-08-26 17:22:00 +0200

    Add files via upload

---
+++ day05.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Aug 26 16:47:16 2023
+@author: lilian
+"""
+
+import numpy as np
+import altair as alt
+import pandas as pd
+import streamlit as st
+
+st.header("st.write c'est fantastique")
+
+st.write("Hello, *World!* :sunglasses:")
+
+st.write(1234)
+
+df = pd.DataFrame({
+    'premiere colonne' : [1,2,3,4],
+    'deuxième colonne' : [10,20,30,40]
+    })
+st.write(df)
+
+st.write('Ci dessous se trouve une Dataframe: ', df , 'Ci dessus se trouve une Dataframe.' )
+
+df2= pd.DataFrame(
+    np.random.randn(200 ,3 ),
+    columns=['a' ,'b' ,'c'])
+
+c = alt.Chart(df2).mark_circle().encode(
+    x='a' ,y='b', color='c' ,tooltip=['a' ,'b' ,'c'])
+st.write(c)
